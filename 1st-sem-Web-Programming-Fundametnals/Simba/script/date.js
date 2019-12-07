40138fc618d11d5fc68d91d58f30b16830c3c16a
udarensamolet <udarensamolet@yahoo.com>
2019-12-07 07:00:31 +0200

    Commit of course project

---
+++ 1st-sem-Web-Programming-Fundametnals/Simba/script/date.js
@@ -0,0 +1,7 @@
+
+	var currentTime = new Date()
+	var month = currentTime.getMonth() + 1
+	var day = currentTime.getDate()
+	var year = currentTime.getFullYear()
+	document.write(day + "/" + month + "/"   + year)
+	
